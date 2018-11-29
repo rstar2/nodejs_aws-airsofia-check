48edfeb71206a54f7c2093885dd3f356150a0187
Rumen Neshev <rstar2@abv.bg>
2018-11-29 23:54:20 +0200

    asd

--- handler.js
+++ handler.js
@@ -1,14 +1,26 @@
-'use strict';
+const dateFormat = require('date-fns/format');
 
-module.exports.hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+// initialize a Twilio client to send SMS
+const twilio = require('.lib/twilio')(process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_AUTH_TOKEN, process.env.TWILIO_SENDER);
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.check = async (event, context, callback) => {
+    // console.log("Event:");
+    // console.dir(event);
+
+    let response;
+    console.time('Invoking function check took');
+
+    try {
+        await twilio.sendSMS(process.env.TWILIO_RECEIVER, response);
+    } catch (e) {
+        console.warn('Failed to send SMS with Twilio Service');
+    }
+
+    console.timeEnd('Invoking function check took');
+    console.log(`Checked on ${dateFormat(Date.now(), 'MMM DD : HH mm')}`);
+    callback(null, response);
 };
+
+
+
