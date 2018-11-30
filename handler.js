45ce31bb3730b212fd1e7cb5cba9ca851c0fbce5
rstar2 <rstar2@abv.bg>
2018-11-30 17:28:39 +0200

    asd

--- handler.js
+++ handler.js
@@ -1,26 +1,43 @@
 const dateFormat = require('date-fns/format');
 
+const luftdaten = require('./lib/luftdaten');
+
 // initialize a Twilio client to send SMS
-const twilio = require('.lib/twilio')(process.env.TWILIO_ACCOUNT_SID,
+const twilio = require('./lib/twilio')(process.env.TWILIO_ACCOUNT_SID,
     process.env.TWILIO_AUTH_TOKEN, process.env.TWILIO_SENDER);
 
-module.exports.check = async (event, context, callback) => {
-    // console.log("Event:");
-    // console.dir(event);
+const LUFTDATEN_NODES = (process.env.LUFTDATEN_NODE || '5545, 10945').split(',')
+    .map(str => +str.trim());
+const LUFTDATEN_CHECK_TYPE = luftdaten.type['PM2.5'];
 
+module.exports.check = async (event, context, callback) => {
     let response;
     console.time('Invoking function check took');
 
-    try {
-        await twilio.sendSMS(process.env.TWILIO_RECEIVER, response);
-    } catch (e) {
-        console.warn('Failed to send SMS with Twilio Service');
+    // start all requests concurrently and wait for tall responses
+    const requests = LUFTDATEN_NODES.map(node => luftdaten.get(node, LUFTDATEN_CHECK_TYPE));
+    const airs = await Promise.all(requests);
+
+    // airs.reduce();
+
+    // TODO: update the DynamoDB last-checked item
+    const isChanged = true;
+
+    // if there's a need to send SMS
+    if (isChanged) {
+        // TODO: Create a proper SMS message
+        // backspace the 'a' date-formatting param
+        response = `Checked on ${dateFormat(Date.now(), 'MMM DD \\at HH:mm')}`;
+
+        try {
+            await twilio.sendSMS(process.env.TWILIO_RECEIVER, response);
+        } catch (e) {
+            console.warn('Failed to send SMS with Twilio Service');
+        }
     }
 
     console.timeEnd('Invoking function check took');
-    console.log(`Checked on ${dateFormat(Date.now(), 'MMM DD : HH mm')}`);
+
     callback(null, response);
 };
-
-
 
